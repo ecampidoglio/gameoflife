c5a42cf55451fbbefa943827149d2645d932f836
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2013-05-17 16:44:28 +0200

    Adds helpers to improve readability of the acceptance tests.

---
+++ Src/AcceptanceTests/Foundation/Assertions.cs
@@ -0,0 +1,14 @@
+﻿namespace Thoughtology.GameOfLife.AcceptanceTests.Foundation
+{
+    using System.Net;
+    using System.Net.Http;
+    using NUnit.Framework;
+
+    public static class Assertions
+    {
+        public static void ShouldBeSuccessful(this HttpResponseMessage response)
+        {
+            response.StatusCode.Should(Is.InRange(HttpStatusCode.OK, HttpStatusCode.MultipleChoices));
+        }
+    }
+}
---
+++ Src/AcceptanceTests/Foundation/HttpExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Thoughtology.GameOfLife.AcceptanceTests.Foundation
+{
+    using System.Net.Http;
+
+    public static class HttpExtensions
+    {
+        public static T ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            return response.Content.ReadAsAsync<T>().Result;
+        }
+    }
+}
