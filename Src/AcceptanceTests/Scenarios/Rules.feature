4513f284149166052760a50add334e9ef349676e
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2013-05-17 16:45:54 +0200

    Adds more specific assertions in the first GoL rule acceptance test.

--- Src/AcceptanceTests/Scenarios/Rules.feature
+++ Src/AcceptanceTests/Scenarios/Rules.feature
@@ -6,4 +6,6 @@
 Scenario: Death by under-population
     Given a live cell has fewer than 2 live neighbours
     When I ask for the next generation of cells
-    Then the cell should be dead
+    Then I should get back a new generation
+    And it should have the same number of cells
+    And the cell should be dead
--- Src/AcceptanceTests/Steps/Rules.cs
+++ Src/AcceptanceTests/Steps/Rules.cs
@@ -3,17 +3,18 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Net.Http;
+    using Foundation;
     using Hooks;
     using NUnit.Framework;
     using TechTalk.SpecFlow;
-    using Web.Models;
 
     [Binding]
     public class Rules
     {
         private readonly WebClient client;
-        private Cell seed;
+        private IEnumerable<dynamic> seed;
         private HttpResponseMessage response;
+        private IEnumerable<dynamic> nextGeneration;
 
         public Rules(WebClient client)
         {
@@ -23,26 +24,42 @@
         [Given]
         public void Given_a_live_cell_has_fewer_than_COUNT_live_neighbours(byte count)
         {
-            seed = new Cell(alive: true, neighbours: --count);
+            seed = new[]
+                   {
+                       new { Alive = true, Neighbours = --count }
+                   };
         }
 
         [When]
         public void When_I_ask_for_the_next_generation_of_cells()
         {
-            var request = new[] { seed };
-            response = client.PostAsJson("api/generation", request);
+            response = client.PostAsJson("api/generation", seed);
+        }
+
+        [Then]
+        public void Then_I_should_get_back_a_new_generation()
+        {
+            response.ShouldBeSuccessful();
+            nextGeneration = ParseGenerationFromResponse();
+            nextGeneration.ShouldNot(Be.Null);
+        }
+
+        [Then]
+        public void Then_it_should_have_the_same_number_of_cells()
+        {
+            nextGeneration.Should(Have.Count.EqualTo(1));
         }
 
         [Then]
         public void Then_the_cell_should_be_dead()
         {
-            var generation = ParseGenerationFromResponse();
-            generation.Single().Alive.ShouldBeFalse();
+            var isAlive = (bool)nextGeneration.Single().Alive;
+            isAlive.ShouldBeFalse();
         }
 
-        private IEnumerable<Cell> ParseGenerationFromResponse()
+        private IEnumerable<dynamic> ParseGenerationFromResponse()
         {
-            return response.Content.ReadAsAsync<IEnumerable<Cell>>().Result;
+            return response.ReadContentAs<IEnumerable<dynamic>>();
         }
     }
 }
