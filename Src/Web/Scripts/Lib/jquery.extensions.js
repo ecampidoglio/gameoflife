152ed252eb4145397b6d75436b196e42b6b3b2d6
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2013-05-20 10:58:41 +0200

    Adds the JS libraries to use on the web front-end.

---
+++ Src/Web/Scripts/Lib/jquery.extensions.js
@@ -0,0 +1,21 @@
+﻿jQuery.extend({
+    postJson: function(url, data, callback, errorCallback, completedCallback) {
+        // shift arguments if data argument was omited
+        if (jQuery.isFunction(data)) {
+            type = type || callback;
+            callback = data;
+            data = {};
+        }
+
+        return jQuery.ajax({
+            type: "POST",
+            contentType: "application/json; charset=UTF-8",
+            url: url,
+            data: data,
+            success: callback,
+            error: errorCallback,
+            completed: completedCallback,
+            dataType: "json"
+        });
+    },
+});
